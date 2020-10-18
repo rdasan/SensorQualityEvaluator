d55dd109dfc42c8a8e9e0f2884f37e5e720f1872
rdasan <rejidasan82@gmail.com>
2020-10-18 11:10:18 -0700

    Adding a Test project for the Console App for a more Intergration style test

---
+++ tests/SensorQualityEvaluator.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SensorQualityEvaluator.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
