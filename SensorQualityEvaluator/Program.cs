9b0e48473d3946c5062666cf46d9ebf64febada3
rdasan <rejidasan82@gmail.com>
2020-10-17 17:35:43 -0700

    adding the Console App and the To be wriiten method of the Library

--- SensorQualityEvaluator/Program.cs
+++ SensorQualityEvaluator/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace SensorQualityEvaluator
 {
     class Program
     {
-        static void Main(string[] args)
+        //For Cross Platform compatibility. If this app is run in a docker container,
+        //the specific exit codes can help determine action to be taken within the container
+        private const int SuccessCode = 0;
+        private const int FailureCode = 1;
+
+        static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //The first command line argument should be the file path
+            if (args.Length < 1)
+            {
+                await LogErrorAsync("Missing arguments");
+                return FailureCode;
+            }
+
+            string samplingFilePath = args[0];
+            if (!File.Exists(samplingFilePath))
+            {
+                await LogErrorAsync("File not found");
+                return FailureCode;
+            }
+
+            try
+            {
+                var samplingContent = await File.ReadAllTextAsync(samplingFilePath);
+
+                QualityChecker qualityChecker = new QualityChecker();
+                string sensorQualityReport = qualityChecker.EvaluateLogFile(samplingContent);
+
+                Console.WriteLine(sensorQualityReport);
+
+                return SuccessCode;
+            }
+            catch (Exception ex)
+            {
+                await LogExceptionAsync(ex);
+                return FailureCode;
+            }
+        }
+
+        public static async Task LogExceptionAsync(Exception exception)
+        {
+            await LogErrorAsync($"{exception}", "EXCEPTION");
+        }
+
+        public static async Task LogErrorAsync(string message, string errorType = "ERROR")
+        {
+            //If we have other log providers like NLog, Serilog, we can log to them here
+            await Console.Error.WriteLineAsync($"{errorType} {DateTime.UtcNow} Application: SensorQualityEvaluator, message: {message}");
         }
     }
 }
