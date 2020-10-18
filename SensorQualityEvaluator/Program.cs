b312093fc8f91f99d37252a113de0a9d597f08b8
rdasan <rejidasan82@gmail.com>
2020-10-17 17:29:45 -0700

    Adding the Console App and Library Projects

---
+++ SensorQualityEvaluator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SensorQualityEvaluator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
